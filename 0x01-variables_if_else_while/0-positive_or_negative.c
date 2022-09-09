1e1c4be43f9bc4cb891b18cff3ec14d3613f8206
tobias@288 <tobiasuche7@gmail.com>
2022-09-09 11:17:34 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,17 +5,16 @@
 /**
  * main - Entry point
  *
- * Return: always return (Success)
+ * Return: 0 Always (Success)
  */
 
 int main(void)
-
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	} else if (n == 0)
