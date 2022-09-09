278faf15e5e2cf38efa421e16e952b44c70164e5
tobias@288 <tobiasuche7@gmail.com>
2022-09-08 17:04:39 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
- *  
- * Return: always 0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);     
+
+	return (0);
 }
