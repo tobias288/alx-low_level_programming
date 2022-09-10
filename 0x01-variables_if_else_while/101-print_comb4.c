e7c8455c95c8e229d9580e942b9ccf56f2388891
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-09-10 11:10:22 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,46 +1,49 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print all pssobile different comn=binations of two digits.
+ * Description: print all possible different
+ *            combinations of two digits.
  *
- * Return: Alawys 0 (Success)
- */
+ * Return: Always 0 (Success)
+*/
 
-int main (void)
+int main(void)
 {
-	int sum1 = 0;
-	int sum2, sum3;
+	int digit1 = 0;
+	int digit2, digit3;
 
-	while (sum1 <= 9)
+	while (digit1 <= 9)
 	{
-		sum2 = 0;
-		while (sum2 <= 9)
+		digit2 = 0;
+		while (digit2 <= 9)
 		{
-			sum3 = 0;
-			while (sum3 <= 9)
+			digit3 = 0;
+			while (digit3 <= 9)
 			{
-				if (sum1 != sum2 && sum1 < sum2 && sum2 != sum3 && sum2 < sum3)
+				if (digit1 != digit2 &&
+				    digit1 < digit2 &&
+				    digit2 != digit3 &&
+				    digit2 < digit3)
 				{
-					putchar(sum1 + 48);
-					putchar(sum2 + 48);
-					putchar(sum3 + 48);
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
 
-					if (sum1 + sum2 + sum3 != 24)
+					if (digit1 + digit2 + digit3 != 24)
 					{
-						putchar (',');
-						putchar (' ');
+						putchar(',');
+						putchar(' ');
 					}
 				}
-				++sum3;
+				++digit3;
 			}
-			++sum2;
+			++digit2;
 		}
-		++sum1;
+		++digit1;
 	}
-	putchar ('\n');
+	putchar('\n');
 
 	return (0);
 }
-				 
