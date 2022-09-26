9c925c6c6dbd0f8eb3dd0d7bcbf2ca3366348621
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-09-26 21:25:05 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,57 +1,29 @@
 #include "holberton.h"
 
-
-
 /**
+ * print_chessboard - prints the chess board
  *
- *  * print_chessboard - prints the chess board
+ * @a: input pointer
  *
- *   *
- *
- *    * @a: input pointer
- *
- *     *
- *
- *      * Return: return nothing
- *
- *      */
-
-
+ * Return: return nothing
+*/
 
 void print_chessboard(char (*a)[8])
-
 {
+	int i, j;
 
-		int i, j;
+	i = 0;
+	j = 0;
+	while (i < 64)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar('\n');
+		}
 
-
-
-			i = 0;
-
-				j = 0;
-
-					while (i < 64)
-
-							{
-
-										if (i % 8 == 0 && i != 0)
-
-													{
-
-																	j = i;
-
-																				_putchar('\n');
-
-																						}
-
-
-
-												_putchar(a[i / 8][i - j]);
-
-														i++;
-
-															}
-
-						_putchar('\n');
-
+		_putchar(a[i / 8][i - j]);
+		i++;
+	}
+	_putchar('\n');
 }
