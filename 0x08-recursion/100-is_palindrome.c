43fdc39f619a64082b7c2c6b5c250f1d37c98660
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-09-28 13:34:31 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,99 +1,50 @@
 #include "holberton.h"
-
 /**
- *
- *  * length - helps do it
- *
- *   * @s: string
- *
- *    * Return: string length
- *
- *     */
-
+ * length - helps do it
+ * @s: string
+ * Return: string length
+ */
 int length(char *s)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			if (*s)
-
-					{
-
-								i = i + length(s + 1);
-
-										return (i += 1);
-
-											}
-
-				return (0);
-
+	if (*s)
+	{
+		i = i + length(s + 1);
+		return (i += 1);
+	}
+	return (0);
 }
-
 /**
- *
- *  * helper2 - helps more
- *
- *   * @i: integer i
- *
- *    * @s: string
- *
- *     * Return: int value
- *
- *      */
-
+ * helper2 - helps more
+ * @i: integer i
+ * @s: string
+ * Return: int value
+ */
 int helper2(int i, char *s)
-
 {
-
-		if (*s)
-
-				{
-
-							if (*s != s[length(s) - i])
-
-										{
-
-														return (0);
-
-																}
-
-									else
-
-												{
-
-																return (helper2(i + 1, s + 1));
-
-																		}
-
-										}
-
-			return (1);
-
+	if (*s)
+	{
+		if (*s != s[length(s) - i])
+		{
+			return (0);
+		}
+		else
+		{
+			return (helper2(i + 1, s + 1));
+		}
+	}
+	return (1);
 }
-
 /**
- *
- *  * is_palindrome - is it paldindrome
- *
- *   * @s: string to check
- *
- *    * Return: boolean
- *
- *     */
-
+ * is_palindrome - is it paldindrome
+ * @s: string to check
+ * Return: boolean
+ */
 int is_palindrome(char *s)
-
 {
+	int i = 1;
 
-		int i = 1;
-
-
-
-			return (helper2(i, s));
-
-
+	return (helper2(i, s));
 
 }
