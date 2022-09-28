435d19e6201f86f394f0afafa7fadc12965c7a0c
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-09-28 13:07:47 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,35 +1,18 @@
 #include "holberton.h"
 
-
-
 /**
+ * _print_rev_recursion - prints a string in reverse
  *
- *  * _print_rev_recursion - prints a string in reverse
+ * @s: string
  *
- *   *
- *
- *    * @s: string
- *
- *     *
- *
- *      * Return: nothing
- *
- *      */
-
-
+ * Return: nothing
+*/
 
 void _print_rev_recursion(char *s)
-
 {
-
-		if (*s != '\0')
-
-				{
-
-							_print_rev_recursion(s + 1);
-
-									_putchar(*s);
-
-										}
-
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
