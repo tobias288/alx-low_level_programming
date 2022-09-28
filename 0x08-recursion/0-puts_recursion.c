704aca153a562a9f2917445dc31de4ca3c2178f8
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-09-28 13:01:05 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,39 +1,20 @@
 #include "holberton.h"
 
-
-
 /**
+ * _puts_recursion - a function that prints a new line
  *
- *  * _puts_recursion - a function that prints a new line
+ * @s: the string
  *
- *   *
- *
- *    * @s: the string
- *
- *     *
- *
- *      * Return: nothing
- *
- *      */
-
-
+ * Return: nothing
+*/
 
 void _puts_recursion(char *s)
-
 {
-
-		if (*s != '\0')
-
-				{
-
-							_putchar(*s);
-
-									_puts_recursion(s + 1);
-
-										}
-
-			else
-
-						_putchar('\n');
-
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
