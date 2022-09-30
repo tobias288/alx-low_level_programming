6e62f8f92889d8a3e4ad05fbd039f23cdc37ea83
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-09-30 22:39:59 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,15 @@
-/**
- * main - print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * Return: Always 0 (Success)
- */
-
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * main -  a program that prints the number of arguments passed into it.
+ * @argc: argument counter
+ * @argv: argument value
+ * Return: Always 1 success
+ */
 int main(int argc, char *argv[])
 {
-	(void) argv; /*Ignore argv*/
-	printf("%i\n", argc - 1);
+	if (argv[0])
+		printf("%d\n", argc - 1);
 
 	return (0);
 }
