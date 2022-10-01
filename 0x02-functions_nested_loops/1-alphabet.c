79580d2b64f802f2b4f023207175f60b9d3ce8a4
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-01 10:11:25 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,15 @@
-#include "holberton.h"
+#include"main.h"
 
 /**
- * print_alphabet - prints alphabet
- *
- * Return: always 0;
- *
- */
+ * print_alphabet - utilizes on the _putchar function to print
+ *                 the alphabet a - z
+*/
+
 void print_alphabet(void)
 {
-	char i;
+	int ch;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
 	_putchar('\n');
 }
