d325442df24560c3f5703c101456ee886f855550
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-03 11:20:44 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,83 +1,42 @@
 #include <stdlib.h>
 
-
-
 /**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the array.
+ * @height: height of the array.
  *
- *  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- *
- *   * @width: width of the array.
- *
- *    * @height: height of the array.
- *
- *     *
- *
- *      * Return: pointer of an array of integers
- *
- *       */
-
+ * Return: pointer of an array of integers
+ */
 int **alloc_grid(int width, int height)
-
 {
+	int **gridout;
+	int i, j;
 
-		int **gridout;
+	if (width < 1 || height < 1)
+		return (NULL);
 
-			int i, j;
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
+	{
+		free(gridout);
+		return (NULL);
+	}
 
+	for (i = 0; i < height; i++)
+	{
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
+			return (NULL);
+		}
+	}
 
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
 
-				if (width < 1 || height < 1)
-
-							return (NULL);
-
-
-
-					gridout = malloc(height * sizeof(int *));
-
-						if (gridout == NULL)
-
-								{
-
-											free(gridout);
-
-													return (NULL);
-
-														}
-
-
-
-							for (i = 0; i < height; i++)
-
-									{
-
-												gridout[i] = malloc(width * sizeof(int));
-
-														if (gridout[i] == NULL)
-
-																	{
-
-																					for (i--; i >= 0; i--)
-
-																										free(gridout[i]);
-
-																								free(gridout);
-
-																											return (NULL);
-
-																													}
-
-															}
-
-
-
-								for (i = 0; i < height; i++)
-
-											for (j = 0; j < width; j++)
-
-															gridout[i][j] = 0;
-
-
-
-									return (gridout);
-
+	return (gridout);
 }
