77a18f95dff39aaf93c2ea55dfd2bf6b0f8e242c
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-10 22:51:54 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,37 +1,19 @@
-i#include <stdio.h>
-
+#include <stdio.h>
 #include "dog.h"
 
-
-
 /**
+ * main - check the code for ALX School students.
  *
- *  * main - check the code for ALX School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *    */
-
-
+ * Return: Always 0.
+*/
 
 int main(void)
-
 {
+	struct dog my_dog;
 
-		struct dog my_dog;
-
-
-
-			my_dog.name = "Poppy";
-
-				my_dog.age = 3.5;
-
-					my_dog.owner = "Bob";
-
-						printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-
-							return (0);
-
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
 }
