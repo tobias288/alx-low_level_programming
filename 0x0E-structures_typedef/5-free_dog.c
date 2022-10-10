dddb4bf45c66cc98e8c051e1a8c223ca118c54ba
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-10 23:08:04 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,35 +1,18 @@
 #include "dog.h"
-
 #include <stdlib.h>
 
-
-
 /**
+ * free_dog - frees dogs.
+ * @d: struct dog.
  *
- *  * free_dog - frees dogs.
- *
- *   * @d: struct dog.
- *
- *    *
- *
- *     * Return: no return.
- *
- *      */
-
+ * Return: no return.
+ */
 void free_dog(dog_t *d)
-
 {
-
-		if (d)
-
-				{
-
-							free(d->name);
-
-									free(d->owner);
-
-											free(d);
-
-												}
-
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
