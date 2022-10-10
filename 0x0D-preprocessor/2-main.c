d934638adc176c350616ebe68410affe99b63670
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-10 22:39:09 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: prints the name of the file it was
+ *              compiled from, followed by a new line
  *
- *   *
- *
- *    * Description: prints the name of the file it was
- *
- *     *              compiled from, followed by a new line
- *
- *      *
- *
- *       * Return: Always 0 (Success)
- *
- *       */
-
-
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
-
 {
-
-		printf("%s\n", __FILE__);
-
-			return (0);
-
+	printf("%s\n", __FILE__);
+	return (0);
 }
