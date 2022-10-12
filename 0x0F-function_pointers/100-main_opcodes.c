c00c191a4e53823af2a1ef424f3dc00c42aad89e
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-12 19:21:26 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,77 +1,39 @@
-i#include <stdio.h>
-
+#include <stdio.h>
 #include <stdlib.h>
 
-
-
 /**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- *  * main - check the code for Holberton School students.
- *
- *   * @argc: argument count.
- *
- *    * @argv: argument vector.
- *
- *     *
- *
- *      * Return: Always 0.
- *
- *       */
-
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
-
 {
+	char *opc = (char *) main;
+	int i, nbytes;
 
-		char *opc = (char *) main;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-			int i, nbytes;
+	nbytes = atoi(argv[1]);
 
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
 
-				if (argc != 2)
-
-						{
-
-									printf("Error\n");
-
-											exit(1);
-
-												}
-
-
-
-					nbytes = atoi(argv[1]);
-
-
-
-						if (nbytes < 0)
-
-								{
-
-											printf("Error\n");
-
-													exit(2);
-
-														}
-
-
-
-							for (i = 0; i < nbytes; i++)
-
-									{
-
-												printf("%02x", opc[i] & 0xFF);
-
-														if (i != nbytes - 1)
-
-																		printf(" ");
-
-															}
-
-
-
-								printf("\n");
-
-									return (0);
-
+	printf("\n");
+	return (0);
 }
