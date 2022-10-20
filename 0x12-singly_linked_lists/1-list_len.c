a8471fc34b663b992d28d3f2e8e531fff4987360
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-20 21:58:13 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,37 +1,19 @@
-i#include "lists.h"
-
+#include "lists.h"
 /**
- *
- *  * list_len - returns then number of elements in a list.
- *
- *   * @h: singly linked list.
- *
- *    * Return: number of elements in the list.
- *
- *     */
-
-
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
 
 size_t list_len(const list_t *h)
-
 {
+	size_t nelem;
 
-		size_t nelem;
-
-
-
-			nelem = 0;
-
-				while (h != NULL)
-
-						{
-
-									h = h->next;
-
-											nelem++;
-
-												}
-
-					return (nelem);
-
+	nelem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
