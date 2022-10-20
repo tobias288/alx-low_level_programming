17177afe0e9ad91b1205e34e8bf57116a180ba8f
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-20 22:05:38 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,81 +1,41 @@
-i#include "lists.h"
-
-
+#include "lists.h"
 
 /**
- *
- *  * add_node_end - adds a new node at the end
- *
- *   * of a list_t list.
- *
- *    * @head: head of the linked list.
- *
- *     * @str: string to store in the list.
- *
- *      * Return: address of the head.
- *
- *       */
-
-
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
 
 list_t *add_node_end(list_t **head, const char *str)
-
 {
+	list_t *new, *temp;
+	size_t nchar;
 
-		list_t *new, *temp;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-			size_t nchar;
+	new->str = strdup(str);
 
+	for (nchar = 0; str[nchar]; nchar++)
+		;
 
+	new->len = nchar;
+	new->next = NULL;
+	temp = *head;
 
-				new = malloc(sizeof(list_t));
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
 
-					if (new == NULL)
-
-								return (NULL);
-
-
-
-						new->str = strdup(str);
-
-
-
-							for (nchar = 0; str[nchar]; nchar++)
-
-										;
-
-
-
-								new->len = nchar;
-
-									new->next = NULL;
-
-										temp = *head;
-
-
-
-											if (temp == NULL)
-
-													{
-
-																*head = new;
-
-																	}
-
-												else
-
-														{
-
-																	while (temp->next != NULL)
-
-																					temp = temp->next;
-
-																			temp->next = new;
-
-																				}
-
-
-
-													return (*head);
-
+	return (*head);
 }
