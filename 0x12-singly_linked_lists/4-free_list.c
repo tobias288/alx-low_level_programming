4c304513f4811e9922ad7fe7d1c2acc17e0df242
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-20 22:10:00 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,37 +1,19 @@
-i#include "lists.h"
-
-
+#include "lists.h"
 
 /**
- *
- *  * free_list - frees a list
- *
- *   * @head: head of the linked list.
- *
- *    * Return: no return.
- *
- *     */
-
-
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
 
 void free_list(list_t *head)
-
 {
+	list_t *current;
 
-		list_t *current;
-
-
-
-			while ((current = head) != NULL)
-
-					{
-
-								head = head->next;
-
-										free(current->str);
-
-												free(current);
-
-													}
-
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
