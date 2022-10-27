9789c83152407991bd47fd1bbb8f2ebf4d5349e1
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-27 17:53:37 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,14 @@
-hhfjj
+/**
+ * get_endianness - a function that checks if
+ *                  architecture is little endian
+ *                  or big endian
+ *
+ * Return: 1 if little endian or 0 if big endian
+*/
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *c = (char *) &num;
+
+	return (c[0]);
+}
