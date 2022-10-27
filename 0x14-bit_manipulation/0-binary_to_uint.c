cde982eef4895d3f748cdf8f1d8a58b8485122f1
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-27 15:26:22 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,67 +1,37 @@
-nary_to_uint - converts a binary number to an
+#include "holberton.h"
 
+/**
+ * binary_to_uint - converts a binary number to an
  * unsigned int.
+ * @b: binary.
  *
- *  * @b: binary.
- *
- *   *
- *
- *    * Return: unsigned int.
- *
- *     */
-
- unsigned int binary_to_uint(const char *b)
-
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
 {
+	unsigned int ui;
+	int len, base_two;
 
-		unsigned int ui;
+	if (!b)
+		return (0);
 
-			int len, base_two;
+	ui = 0;
 
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
 
-				if (!b)
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
 
-							return (0);
-
-
-
-					ui = 0;
-
-
-
-						for (len = 0; b[len] != '\0'; len++)
-
-									;
-
-
-
-							for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-
-									{
-
-												if (b[len] != '0' && b[len] != '1')
-
-															{
-
-																			return (0);
-
-																					}
-
-
-
-														if (b[len] & 1)
-
-																	{
-
-																					ui += base_two;
-
-																							}
-
-															}
-
-
-
-								return (ui);
-
+	return (ui);
 }
