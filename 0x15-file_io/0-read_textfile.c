813683f9175db8a8b35ee023e3cfd02cab14fa87
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-31 17:06:36 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,71 +1,36 @@
 #include "holberton.h"
 
-
-
 /**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- *  * read_textfile - reads a text file and prints the letters
- *
- *   * @filename: filename.
- *
- *    * @letters: numbers of letters printed.
- *
- *     *
- *
- *      * Return: numbers of letters printed. It fails, returns 0.
- *
- *       */
-
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-		int fd;
+	if (!filename)
+		return (0);
 
-			ssize_t nrd, nwr;
+	fd = open(filename, O_RDONLY);
 
-				char *buf;
+	if (fd == -1)
+		return (0);
 
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-					if (!filename)
+	close(fd);
 
-								return (0);
+	free(buf);
 
-
-
-						fd = open(filename, O_RDONLY);
-
-
-
-							if (fd == -1)
-
-										return (0);
-
-
-
-								buf = malloc(sizeof(char) * (letters));
-
-									if (!buf)
-
-												return (0);
-
-
-
-										nrd = read(fd, buf, letters);
-
-											nwr = write(STDOUT_FILENO, buf, nrd);
-
-
-
-												close(fd);
-
-
-
-													free(buf);
-
-
-
-														return (nwr);
-
+	return (nwr);
 }
