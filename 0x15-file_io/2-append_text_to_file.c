a46838595836ff02fa3380fe36930e35bfb21f0e
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-31 17:07:51 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,77 +1,39 @@
 #include "holberton.h"
 
-
-
 /**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
  *
- *  * append_text_to_file - appends text at the end of a file
- *
- *   * @filename: filename.
- *
- *    * @text_content: added content.
- *
- *     *
- *
- *      * Return: 1 if the file exists. -1 if the fails does not exist
- *
- *       * or if it fails.
- *
- *        */
-
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
+ */
 int append_text_to_file(const char *filename, char *text_content)
-
 {
+	int fd;
+	int nletters;
+	int rwr;
 
-		int fd;
+	if (!filename)
+		return (-1);
 
-			int nletters;
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-				int rwr;
+	if (fd == -1)
+		return (-1);
 
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
+		rwr = write(fd, text_content, nletters);
 
-					if (!filename)
+		if (rwr == -1)
+			return (-1);
+	}
 
-								return (-1);
+	close(fd);
 
-
-
-						fd = open(filename, O_WRONLY | O_APPEND);
-
-
-
-							if (fd == -1)
-
-										return (-1);
-
-
-
-								if (text_content)
-
-										{
-
-													for (nletters = 0; text_content[nletters]; nletters++)
-
-																	;
-
-
-
-															rwr = write(fd, text_content, nletters);
-
-
-
-																	if (rwr == -1)
-
-																					return (-1);
-
-																		}
-
-
-
-									close(fd);
-
-
-
-										return (1);
-
+	return (1);
 }
