d7cae557d65345a82d5332a91eb4229bd3fe2949
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-10-31 17:07:15 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,75 +1,38 @@
 #include "holberton.h"
 
-
-
 /**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- *  * create_file - creates a file
- *
- *   * @filename: filename.
- *
- *    * @text_content: content writed in the file.
- *
- *     *
- *
- *      * Return: 1 if it success. -1 if it fails.
- *
- *       */
-
+ * Return: 1 if it success. -1 if it fails.
+ */
 int create_file(const char *filename, char *text_content)
-
 {
+	int fd;
+	int nletters;
+	int rwr;
 
-		int fd;
+	if (!filename)
+		return (-1);
 
-			int nletters;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-				int rwr;
+	if (fd == -1)
+		return (-1);
 
+	if (!text_content)
+		text_content = "";
 
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-					if (!filename)
+	rwr = write(fd, text_content, nletters);
 
-								return (-1);
+	if (rwr == -1)
+		return (-1);
 
+	close(fd);
 
-
-						fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-
-
-							if (fd == -1)
-
-										return (-1);
-
-
-
-								if (!text_content)
-
-											text_content = "";
-
-
-
-									for (nletters = 0; text_content[nletters]; nletters++)
-
-												;
-
-
-
-										rwr = write(fd, text_content, nletters);
-
-
-
-											if (rwr == -1)
-
-														return (-1);
-
-
-
-												close(fd);
-
-
-
-													return (1);
-
+	return (1);
 }
