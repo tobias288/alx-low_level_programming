fad521f5d729abaadb4c348b8aacd9244f767041
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-12-09 13:40:40 +0100

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,77 +1,39 @@
 #include "lists.h"
 
-
-
 /**
+ * add_dnodeint_end - adds a new node at the end
+ * of a dlistint_t list
  *
- *  * add_dnodeint_end - adds a new node at the end
- *
- *   * of a dlistint_t list
- *
- *    *
- *
- *     * @head: head of the list
- *
- *      * @n: value of the element
- *
- *       * Return: the address of the new element
- *
- *        */
-
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-
 {
+	dlistint_t *h;
+	dlistint_t *new;
 
-		dlistint_t *h;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-			dlistint_t *new;
+	new->n = n;
+	new->next = NULL;
 
+	h = *head;
 
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new;
+	}
+	else
+	{
+		*head = new;
+	}
 
-				new = malloc(sizeof(dlistint_t));
+	new->prev = h;
 
-					if (new == NULL)
-
-								return (NULL);
-
-
-
-						new->n = n;
-
-							new->next = NULL;
-
-
-
-								h = *head;
-
-
-
-									if (h != NULL)
-
-											{
-
-														while (h->next != NULL)
-
-																		h = h->next;
-
-																h->next = new;
-
-																	}
-
-										else
-
-												{
-
-															*head = new;
-
-																}
-
-
-
-											new->prev = h;
-
-
-
-												return (new);
-
+	return (new);
 }
