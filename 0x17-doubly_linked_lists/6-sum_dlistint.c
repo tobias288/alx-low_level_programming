f27187b6dd94af9cb35f5b423fcc2d84fae4cb5e
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-12-09 13:42:35 +0100

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,57 +1,29 @@
 #include "lists.h"
 
-
-
 /**
+ * sum_dlistint - returns the sum of all the data (n)
+ * of a doubly linked list
  *
- *  * sum_dlistint - returns the sum of all the data (n)
- *
- *   * of a doubly linked list
- *
- *    *
- *
- *     * @head: head of the list
- *
- *      * Return: sum of the data
- *
- *       */
-
+ * @head: head of the list
+ * Return: sum of the data
+ */
 int sum_dlistint(dlistint_t *head)
-
 {
+	int sum;
 
-		int sum;
+	sum = 0;
 
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
 
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
 
-			sum = 0;
-
-
-
-				if (head != NULL)
-
-						{
-
-									while (head->prev != NULL)
-
-													head = head->prev;
-
-
-
-											while (head != NULL)
-
-														{
-
-																		sum += head->n;
-
-																					head = head->next;
-
-																							}
-
-												}
-
-
-
-					return (sum);
-
+	return (sum);
 }
