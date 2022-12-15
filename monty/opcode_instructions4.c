59ad57eb89bbf63f620c6ff1c0ce5896b23b860c
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-12-15 22:20:27 +0100

    Update opcode_instructions4.c

--- monty/opcode_instructions4.c
+++ monty/opcode_instructions4.c
@@ -1,125 +1,63 @@
 #include "monty.h"
 
-
-
 /**
+ * _rotl - rotates the first element to the bottom and  the second to the top
  *
- *  * _rotl - rotates the first element to the bottom and  the second to the top
- *
- *   *
- *
- *    * @doubly: head of the linked list
- *
- *     * @cline: line number;
- *
- *      * Return: no return
- *
- *       */
-
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
 void _rotl(stack_t **doubly, unsigned int cline)
-
 {
+	stack_t *aux1 = NULL;
+	stack_t *aux2 = NULL;
+	(void)cline;
 
-		stack_t *aux1 = NULL;
+	if (*doubly == NULL)
+		return;
 
-			stack_t *aux2 = NULL;
+	if ((*doubly)->next == NULL)
+		return;
 
-				(void)cline;
+	aux1 = (*doubly)->next;
+	aux2 = *doubly;
 
+	for (; aux2->next != NULL; aux2 = aux2->next)
+		;
 
-
-					if (*doubly == NULL)
-
-								return;
-
-
-
-						if ((*doubly)->next == NULL)
-
-									return;
-
-
-
-							aux1 = (*doubly)->next;
-
-								aux2 = *doubly;
-
-
-
-									for (; aux2->next != NULL; aux2 = aux2->next)
-
-												;
-
-
-
-										aux1->prev = NULL;
-
-											aux2->next = *doubly;
-
-												(*doubly)->next = NULL;
-
-													(*doubly)->prev = aux2;
-
-														*doubly = aux1;
-
+	aux1->prev = NULL;
+	aux2->next = *doubly;
+	(*doubly)->next = NULL;
+	(*doubly)->prev = aux2;
+	*doubly = aux1;
 }
 
-
-
 /**
+ * _rotr - reverse the stack
  *
- *  * _rotr - reverse the stack
- *
- *   *
- *
- *    * @doubly: head of the linked list
- *
- *     * @cline: line number
- *
- *      * Return: no return
- *
- *       */
-
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
 void _rotr(stack_t **doubly, unsigned int cline)
-
 {
+	stack_t *aux = NULL;
+	(void)cline;
 
-		stack_t *aux = NULL;
+	if (*doubly == NULL)
+		return;
 
-			(void)cline;
+	if ((*doubly)->next == NULL)
+		return;
 
+	aux = *doubly;
 
+	for (; aux->next != NULL; aux = aux->next)
+		;
 
-				if (*doubly == NULL)
-
-							return;
-
-
-
-					if ((*doubly)->next == NULL)
-
-								return;
-
-
-
-						aux = *doubly;
-
-
-
-							for (; aux->next != NULL; aux = aux->next)
-
-										;
-
-
-
-								aux->prev->next = NULL;
-
-									aux->next = *doubly;
-
-										aux->prev = NULL;
-
-											(*doubly)->prev = aux;
-
-												*doubly = aux;
-
+	aux->prev->next = NULL;
+	aux->next = *doubly;
+	aux->prev = NULL;
+	(*doubly)->prev = aux;
+	*doubly = aux;
 }
