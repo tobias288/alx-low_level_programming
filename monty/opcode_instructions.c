7da06e5e82f5c61997184cec4573331630e56fb0
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-12-15 22:18:16 +0100

    Update opcode_instructions.c

--- monty/opcode_instructions.c
+++ monty/opcode_instructions.c
@@ -1,271 +1,136 @@
 #include "monty.h"
 
-
-
 /**
+ * _push - pushes an element to the stack
  *
- *  * _push - pushes an element to the stack
- *
- *   *
- *
- *    * @doubly: head of the linked list
- *
- *     * @cline: line number
- *
- *      * Return: no return
- *
- *       */
-
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
 void _push(stack_t **doubly, unsigned int cline)
-
 {
+	int n, j;
 
-		int n, j;
+	if (!vglo.arg)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "usage: push integer\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
 
+	for (j = 0; vglo.arg[j] != '\0'; j++)
+	{
+		if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
+		{
+			dprintf(2, "L%u: ", cline);
+			dprintf(2, "usage: push integer\n");
+			free_vglo();
+			exit(EXIT_FAILURE);
+		}
+	}
 
+	n = atoi(vglo.arg);
 
-			if (!vglo.arg)
-
-					{
-
-								dprintf(2, "L%u: ", cline);
-
-										dprintf(2, "usage: push integer\n");
-
-												free_vglo();
-
-														exit(EXIT_FAILURE);
-
-															}
-
-
-
-				for (j = 0; vglo.arg[j] != '\0'; j++)
-
-						{
-
-									if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
-
-												{
-
-																dprintf(2, "L%u: ", cline);
-
-																			dprintf(2, "usage: push integer\n");
-
-																						free_vglo();
-
-																									exit(EXIT_FAILURE);
-
-																											}
-
-										}
-
-
-
-					n = atoi(vglo.arg);
-
-
-
-						if (vglo.lifo == 1)
-
-									add_dnodeint(doubly, n);
-
-							else
-
-										add_dnodeint_end(doubly, n);
-
+	if (vglo.lifo == 1)
+		add_dnodeint(doubly, n);
+	else
+		add_dnodeint_end(doubly, n);
 }
 
-
-
 /**
+ * _pall - prints all values on the stack
  *
- *  * _pall - prints all values on the stack
- *
- *   *
- *
- *    * @doubly: head of the linked list
- *
- *     * @cline: line numbers
- *
- *      * Return: no return
- *
- *       */
-
+ * @doubly: head of the linked list
+ * @cline: line numbers
+ * Return: no return
+ */
 void _pall(stack_t **doubly, unsigned int cline)
-
 {
+	stack_t *aux;
+	(void)cline;
 
-		stack_t *aux;
+	aux = *doubly;
 
-			(void)cline;
-
-
-
-				aux = *doubly;
-
-
-
-					while (aux)
-
-							{
-
-										printf("%d\n", aux->n);
-
-												aux = aux->next;
-
-													}
-
+	while (aux)
+	{
+		printf("%d\n", aux->n);
+		aux = aux->next;
+	}
 }
 
-
-
 /**
+ * _pint - prints the value at the top of the stack
  *
- *  * _pint - prints the value at the top of the stack
- *
- *   *
- *
- *    * @doubly: head of the linked list
- *
- *     * @cline: line number
- *
- *      * Return: no return
- *
- *       */
-
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
 void _pint(stack_t **doubly, unsigned int cline)
-
 {
+	(void)cline;
 
-		(void)cline;
+	if (*doubly == NULL)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "can't pint, stack empty\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
 
-
-
-			if (*doubly == NULL)
-
-					{
-
-								dprintf(2, "L%u: ", cline);
-
-										dprintf(2, "can't pint, stack empty\n");
-
-												free_vglo();
-
-														exit(EXIT_FAILURE);
-
-															}
-
-
-
-				printf("%d\n", (*doubly)->n);
-
+	printf("%d\n", (*doubly)->n);
 }
 
-
-
 /**
+ * _pop - removes the top element of the stack
  *
- *  * _pop - removes the top element of the stack
- *
- *   *
- *
- *    * @doubly: head of the linked list
- *
- *     * @cline: line number
- *
- *      * Return: no return
- *
- *       */
-
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
 void _pop(stack_t **doubly, unsigned int cline)
-
 {
+	stack_t *aux;
 
-		stack_t *aux;
-
-
-
-			if (doubly == NULL || *doubly == NULL)
-
-					{
-
-								dprintf(2, "L%u: can't pop an empty stack\n", cline);
-
-										free_vglo();
-
-												exit(EXIT_FAILURE);
-
-													}
-
-				aux = *doubly;
-
-					*doubly = (*doubly)->next;
-
-						free(aux);
-
+	if (doubly == NULL || *doubly == NULL)
+	{
+		dprintf(2, "L%u: can't pop an empty stack\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	free(aux);
 }
 
-
-
 /**
+ * _swap - swaps the top two elements of the stack
  *
- *  * _swap - swaps the top two elements of the stack
- *
- *   *
- *
- *    * @doubly: head of the linked list
- *
- *     * @cline: line number
- *
- *      * Return: no return
- *
- *       */
-
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
 void _swap(stack_t **doubly, unsigned int cline)
-
 {
+	int m = 0;
+	stack_t *aux = NULL;
 
-		int m = 0;
+	aux = *doubly;
 
-			stack_t *aux = NULL;
+	for (; aux != NULL; aux = aux->next, m++)
+		;
 
+	if (m < 2)
+	{
+		dprintf(2, "L%u: can't swap, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
 
-
-				aux = *doubly;
-
-
-
-					for (; aux != NULL; aux = aux->next, m++)
-
-								;
-
-
-
-						if (m < 2)
-
-								{
-
-											dprintf(2, "L%u: can't swap, stack too short\n", cline);
-
-													free_vglo();
-
-															exit(EXIT_FAILURE);
-
-																}
-
-
-
-							aux = *doubly;
-
-								*doubly = (*doubly)->next;
-
-									aux->next = (*doubly)->next;
-
-										aux->prev = *doubly;
-
-											(*doubly)->next = aux;
-
-												(*doubly)->prev = NULL;
-
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	aux->next = (*doubly)->next;
+	aux->prev = *doubly;
+	(*doubly)->next = aux;
+	(*doubly)->prev = NULL;
 }
